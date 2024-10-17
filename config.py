573f1606de44c6fc5ffcc1487ed96d1567aeadfe
pywave5 <pywave@mail.ru>
2024-10-18 03:49:07 +0500

    update to 3.0.0

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 """
     info bot for your servers
     author: @pywave
-    version: 2.5.1
+    version: 3.0.0
 """
 
-TOKEN_API = "" # YOUR BOT TOKEN
+TOKEN_API = "BOT_TOKEN" # YOUR BOT TOKEN
 
 # LIST YOUR GAME SERVERS
 SERVERS = [
-    {"ip": "example.com", "port": 27015},
-    {"ip": "example.com", "port": 27016}
+    {"ip": "127.0.0.1", "port": 27015},
+    # {"ip": "127.0.0.1", "port": 27016}
 ]
 
 # defines the delay time in seconds before messages are automatically deleted.
@@ -17,3 +17,6 @@ AUTO_DELETE_DELAY_TIME = 300
 
 # If specified, the bot will restrict access to users who are not members of the group or channel.
 CHAT_ID: int | None = None # Example: -1002281063512 (@username_to_id_bot)
+
+# admins ids for your bot
+ADMINS_IDS = [] # username_to_id_bot
